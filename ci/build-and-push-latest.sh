527441439fde30eb6c0833441f85ddbcf5dfeee2
max.bender1 <max.bender1@ibm.com>
2020-06-30 02:05:21 -0400

    ci/build-and-push-latest:

--- ci/build-and-push-latest.sh
+++ ci/build-and-push-latest.sh
@@ -4,3 +4,11 @@ if [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
     make docker:tag && \
     make docker:push
 fi
+
+if [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then 
+    export DOCKER_BUILD_TAG=$DOCKER_BUILD_TAG-pr && \
+    make docker:build && \
+    make docker:login && \
+    make docker:tag && \
+    make docker:push
+fi
